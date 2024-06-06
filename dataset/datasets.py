67996775ec5fb91fc06e30c2c2124a0e993fd5bf
Max Rosenblattl <max.rosenblattl@tum.de>
2024-06-06 20:17:33 +0000

    Implement automatic dataset creation

--- dataset/datasets.py
+++ dataset/datasets.py
@@ -23,8 +23,18 @@ class PointDetailDataset(Dataset):
 
         file = self.files[idx]
         data = np.load(os.path.join(self.root_dir, file))
-        x = data[0]
-        y = data[1]
+
+        indices = np.random.choice(data.shape[0], 2048, replace=False)
+        y = data[indices].astype(np.float32)
+
+        mean = 0
+        std = 0.01
+        noise = np.random.normal(mean, std, y.shape).astype(np.float32)
+
+        x = y + noise
+
+        print(x.dtype)
+        print(y.dtype)
 
         if self.transform:
             data = self.transform(data)

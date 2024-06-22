f7ffce7edda8faf2b259dd2c90c848fa97c78d66
Max Rosenblattl <max.rosenblattl@tum.de>
2024-06-22 12:11:43 +0200

    Adapt SharedMLP

--- modules/shared_mlp.py
+++ modules/shared_mlp.py
@@ -9,35 +9,25 @@ class Swish(nn.Module):
         return  x * torch.sigmoid(x)
 
 class SharedMLP(nn.Module):
-    def __init__(self, in_channels, out_channels, dim=1, activations=None, use_bn=True):
+    def __init__(self, in_channels, out_channels, dim=1):
         super().__init__()
         if dim == 1:
             conv = nn.Conv1d
-            bn = nn.GroupNorm
+            bn = nn.BatchNorm1d
         elif dim == 2:
             conv = nn.Conv2d
-            bn = nn.GroupNorm
+            bn = nn.BatchNorm2d
         else:
             raise ValueError
-        
-
         if not isinstance(out_channels, (list, tuple)):
             out_channels = [out_channels]
-
-        # Default activations
-        activations = activations or [Swish()] * len(out_channels)
-
         layers = []
-        for oc, activation in zip(out_channels, activations):
+        for oc in out_channels:
             layers.extend([
                 conv(in_channels, oc, 1),
-                #bn(min(8, oc), oc),
-                #Swish(),
+                bn(oc),
+                nn.ReLU(True),
             ])
-            if use_bn:
-                layers.append(bn(min(8, oc), oc))
-            if activation is not None:
-                layers.append(activation)
             in_channels = oc
         self.layers = nn.Sequential(*layers)
 
